6e4782f6c2561782e7741b80e8ef0a11cff3fd03
Juan Pablo Huici <jphuici1@gmail.com>
2022-07-06 18:34:32 -0300

    landbot added

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,3 +57,22 @@ sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
 sr.reveal(`.profile__buttons`, {delay: 800})
 sr.reveal(`.filters__content`, {delay: 900})
 sr.reveal(`.filters`, {delay: 1000})
+
+
+/*== LANDBOT ONLINE INTERVIEW ==*/
+window.addEventListener('mouseover', initLandbot, { once: true });
+window.addEventListener('touchstart', initLandbot, { once: true });
+var myLandbot;
+function initLandbot() {
+  if (!myLandbot) {
+    var s = document.createElement('script');s.type = 'text/javascript';s.async = true;
+    s.addEventListener('load', function() {
+      myLandbot = new Landbot.Popup({
+        configUrl: 'https://landbot.site/v3/H-1298183-FUEBCLL0TE7UT1G5/index.json',
+      });
+    });
+    s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.js';
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+  }
+}
